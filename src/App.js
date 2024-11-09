1ded531d8443dcb2e203524d6a5f0aada3690d8c
Saidislombek-Abdumalikov <saidislomabdumalikov1234@gmail.com>
2024-11-09 11:27:54 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Our School Website</h1>
+      <p>This is a sample school website ready for deployment on GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
